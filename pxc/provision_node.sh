6bdcb8da5b3f6c5093f4d396de93cfeb021da44a
ぐりあんどろ <guriandoro@gmail.com>
2020-04-02 20:19:16 -0300

    Moved sysbench install after repo

--- pxc/provision_node.sh
+++ pxc/provision_node.sh
@@ -2,12 +2,12 @@
 
 #yum -y install tar gdb strace vim qpress socat git wget
 #yum -y groupinstall "Development Tools"
-yum -y install vim socat qpress sysbench
+yum -y install vim socat qpress
 
 yum -y install https://repo.percona.com/yum/percona-release-latest.noarch.rpm
 percona-release enable-only pxc-80 experimental
 percona-release enable tools release
-yum -y install percona-xtradb-cluster
+yum -y install percona-xtradb-cluster sysbench
 
 iptables -F
 setenforce 0

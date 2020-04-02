27b55976f2efff7c592372fa0f208ad297a8f39b
guriandoro <guriandoro@gmail.com>
2020-04-02 22:52:05 +0000

    Added PXC 8.0 testing functionality

--- pxc/provision_node.sh
+++ pxc/provision_node.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-yum -y install http://www.percona.com/downloads/percona-release/redhat/0.1-4/percona-release-0.1-4.noarch.rpm
-yum -y install tar gdb strace vim qpress socat Percona-XtraDB-Cluster-server-57
 
-#yum -y install tar gdb strace vim qpress socat Percona-XtraDB-Cluster-client-57-5.7.16-27.19.1.el7.x86_64 Percona-XtraDB-Cluster-server-57-5.7.16-27.19.1.el7.x86_64 Percona-XtraDB-Cluster-shared-57-5.7.16-27.19.1.el7.x86_64 Percona-XtraDB-Cluster-shared-compat-57-5.7.16-27.19.1.el7.x86_64
-#yum -y install tar gdb strace vim qpress socat Percona-XtraDB-Cluster-client-57-5.7.17-27.20.2.el7.x86_64 Percona-XtraDB-Cluster-server-57-5.7.17-27.20.2.el7.x86_64 Percona-XtraDB-Cluster-shared-57-5.7.17-27.20.2.el7.x86_64 Percona-XtraDB-Cluster-shared-compat-57-5.7.17-27.20.2.el7.x86_64 
-#yum install -y tar gdb strace vim qpress socat Percona-XtraDB-Cluster-client-57-5.7.19-29.22.3.el7.x86_64 Percona-XtraDB-Cluster-server-57-5.7.19-29.22.3.el7.x86_64 Percona-XtraDB-Cluster-shared-57-5.7.19-29.22.3.el7.x86_64 Percona-XtraDB-Cluster-shared-compat-57-5.7.19-29.22.3.el7.x86_64 
+#yum -y install tar gdb strace vim qpress socat git wget
+#yum -y groupinstall "Development Tools"
+yum -y install vim socat qpress sysbench
+
+yum -y install https://repo.percona.com/yum/percona-release-latest.noarch.rpm
+percona-release enable-only pxc-80 experimental
+percona-release enable tools release
+yum -y install percona-xtradb-cluster
+
 iptables -F
 setenforce 0
 mysqld --initialize-insecure --user=mysql
@@ -37,14 +41,17 @@ wsrep_slave_threads = 1
 wsrep_auto_increment_control        = ON
 
 wsrep_sst_method=xtrabackup-v2
-wsrep_sst_auth=root:sekret
+
+## deprecated:
+#wsrep_sst_auth=root:sekret
 
 wsrep_cluster_address=gcomm://$IPS_COMMA
 wsrep_node_address=$NODE_IP
 wsrep_node_name=node$NODE_NR
 
+## deprecated:
+#innodb_locks_unsafe_for_binlog=1
 
-innodb_locks_unsafe_for_binlog=1
 innodb_autoinc_lock_mode=2
 innodb_file_per_table=1
 innodb-log-file-size = 256M
@@ -58,7 +65,7 @@ log_slave_updates
 enforce_gtid_consistency=1
 gtid_mode=on
 
-#pxc-encrypt-cluster-traffic = ON
+pxc-encrypt-cluster-traffic = OFF
 #early-plugin-load=keyring_file.so
 #keyring-file-data=/var/lib/mysql-keyring/keyring
 
@@ -75,21 +82,47 @@ EOF
 
 if [[ $NODE_NR -eq 1 ]]
 then
-	systemctl start mysql@bootstrap
+  systemctl start mysql@bootstrap
 
-	mysql -e "grant all privileges on *.* to 'root'@'192.%' identified by 'sekret';"
-	mysql -e "grant all privileges on *.* to 'root'@'127.0.0.1' identified by 'sekret';"
-	mysql -e "grant all privileges on *.* to 'root'@'localhost' identified by 'sekret';"
+        mysql -e "create user 'root'@'192.%' identified by 'sekret'"
+        mysql -e "create user 'roott'@'localhost' identified with mysql_native_password by 'sekret'"
+        mysql -e "create user 'root'@'127.0.0.1' identified by 'sekret'"
+  mysql -e "grant all privileges on *.* to 'root'@'192.%'"
+  mysql -e "grant all privileges on *.* to 'root'@'127.0.0.1'"
+  mysql -e "grant all privileges on *.* to 'root'@'localhost'"
+        mysql -e "grant all privileges on *.* to 'roott'@'localhost'"
 else
-	for i in {1..60}
-	do
-		MYSQLADMIN=`mysqladmin -uroot -psekret -h$BOOTSTRAP_IP ping`
-		if [[ "$MYSQLADMIN" == "mysqld is alive" ]]
-		then 
-			systemctl start mysql
-			exit
-		else
-			sleep 5
-		fi
-	done
+  for i in {1..60}
+  do
+    MYSQLADMIN=`mysqladmin -uroot -psekret -h$BOOTSTRAP_IP ping`
+    if [[ "$MYSQLADMIN" == "mysqld is alive" ]]
+    then
+      systemctl start mysql
+      exit
+    else
+      sleep 5
+    fi
+  done
 fi
+
+cat >~/.mysql <<EOF
+[client]
+user=root
+password=sekret
+EOF
+
+cat >~/run_sysbench.sh <<EOF
+#!/bin/bash
+mysql -e "create schema if not exists sbtest"
+
+mysql_socket=/var/lib/mysql/mysql.sock
+for command in cleanup prepare run; do
+  sysbench /usr/share/sysbench/oltp_insert.lua --mysql-socket=\${mysql_socket} \\
+  --mysql-user=roott --mysql-password=sekret --mysql-db=sbtest \\
+  --time=300 --threads=1 --report-interval=1 --tables=10 --db-driver=mysql \\
+  \$command
+done;
+EOF
+
+chmod +x ~/run_sysbench.sh
+
